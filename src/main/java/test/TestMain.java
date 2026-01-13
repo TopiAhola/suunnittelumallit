2eeaa18b7bbc85a07918c4186abce9a4c58180b6
Topi Ahola <topi.ahola@metropolia.fi>
2026-01-13 12:14:25 +0200

    jotain

--- src/main/java/test/TestMain.java
+++ src/main/java/test/TestMain.java
@@ -1,4 +1,29 @@
 package test;
 
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.function.UnaryOperator;
+
 public class TestMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("ASD");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.next();
+        System.out.println(input);
+
+        @FunctionalInterface
+                interface Printer {
+            public void doIt ();
+        }
+
+
+        Printer a = input.equals("a") ?  () -> System.out.println(input)   :  () -> System.out.println("input not a")  ;
+
+       a.doIt();
+    }
+
+
 }
