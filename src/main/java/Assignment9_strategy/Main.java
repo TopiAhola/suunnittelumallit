7231b18e2e9004a3853d93f2c0c7d33c657df059
Topi Ahola <topi.ahola@metropolia.fi>
2026-02-04 15:54:14 +0200

    Assignment 9 in progress

--- src/main/java/Assignment9_strategy/Main.java
+++ src/main/java/Assignment9_strategy/Main.java
@@ -4,26 +4,32 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] intArray = new int[] {1,1897,-22,374,0,4123,54,655,7,86,9,10,54,123,824,3,9999,1};
+        //small array
+        int[] smallIntArray = new int[] {1,1897,-22,374,0,4123,54,655,7,86,9,10,54,123,824,3,9999,1};
         System.out.println("Original array:");
-        System.out.println(Arrays.toString(intArray));
+        System.out.println(Arrays.toString(smallIntArray));
+
+        //large array
+        int[] largeIntArray = new int[1_000_000];
+        for (int i = 0; i < 1_000_000; i++) {
+            largeIntArray[i] = (int)((Math.random() * 1000) - 2000);
+        }
 
         Context context = new Context();
 
         context.setAlgorithm(new BubbleSort());
-        int[] bubbleSorted = context.doSorting(intArray.clone());
+        int[] bubbleSorted = context.doSorting(smallIntArray.clone());
         System.out.println(Arrays.toString(bubbleSorted));
 
         context.setAlgorithm(new InsertionSort());
-        int[] radixSorted = context.doSorting(intArray.clone());
+        int[] radixSorted = context.doSorting(smallIntArray.clone());
         System.out.println(Arrays.toString(radixSorted));
 
         context.setAlgorithm(new QuickSort());
-        int[] quickSorted = context.doSorting(intArray.clone());
+        int[] quickSorted = context.doSorting(smallIntArray.clone());
         System.out.println(Arrays.toString(quickSorted));
 
-        System.out.println("Original array:");
-        System.out.println(Arrays.toString(intArray));
+
 
         //TODO: timers (nanotime)
 
