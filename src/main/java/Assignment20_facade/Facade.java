029f393aa86711798715b873ee7f0123b2a0a098
Topi Ahola <topi.ahola@metropolia.fi>
2026-04-17 23:09:50 +0300

    assignment 20 started

---
+++ src/main/java/Assignment20_facade/Facade.java
@@ -0,0 +1,32 @@
+package Assignment20_facade;
+
+import java.io.IOException;
+
+
+
+
+/**
+ * The method should send a HTTP GET request to the given URL, retrieve the JSON response, and extract the value of the given attribute name from the JSON.
+ *
+ * Should the URL be invalid or the HTTP request fail, the method should throw an IOException.
+ *
+ * If the attribute is not found, the method should throw an IllegalArgumentException. If there are multiple attributes with the same name, the method should return the value of the first one found.
+ *
+ * Finally, demonstrate that the Chuck Norris joke can still be retrieved using your facade. Also, show how the facade works with another API of your choice (e.g. https://api.fxratesapi.com/latest for the latest foreign exchange rates).
+ */
+public class Facade {
+
+
+    private JokeClient jokeClient;
+
+    public Facade(){
+        this.jokeClient = new JokeClient();
+    }
+
+    String getAttributeValueFromJson(String urlString, String attributeName) throws IllegalArgumentException, IOException {
+
+
+    return null;
+    };
+
+}
---
+++ src/main/java/Assignment20_facade/Main.java
@@ -0,0 +1,39 @@
+package Assignment20_facade;
+
+
+import java.io.IOException;
+
+/**
+ * The method should send a HTTP GET request to the given URL, retrieve the JSON response, and extract the value of the given attribute name from the JSON.
+ *
+ * Should the URL be invalid or the HTTP request fail, the method should throw an IOException.
+ *
+ * If the attribute is not found, the method should throw an IllegalArgumentException. If there are multiple attributes with the same name, the method should return the value of the first one found.
+ *
+ * Finally, demonstrate that the Chuck Norris joke can still be retrieved using your facade. Also, show how the facade works with another API of your choice (e.g. https://api.fxratesapi.com/latest for the latest foreign exchange rates).
+ */
+public class Main{
+    public static void main(String[] args) {
+
+        Facade facade = new Facade();
+        try {
+            String result = facade.getAttributeValueFromJson(
+                    "https://api.chucknorris.io/jokes/random",
+                    "value"
+            );
+            System.out.println(result);
+
+
+        } catch (IOException e) {
+            System.out.println("URL is invalid or the HTTP request failed");
+
+        } catch (IllegalArgumentException e2) {
+            System.out.println("Attribute not found in JSON");
+
+        } catch (Exception e3) {
+            e3.printStackTrace();
+        }
+
+
+    }
+}
